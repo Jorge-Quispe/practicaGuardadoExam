d732f5749bd1378549786b8b1b5f65664095d17d
Jorge <jorgequispes@upeu.edu.pe>
2020-10-15 06:33:56 -0500

    practica

---
+++ src/test/java/pe/edu/upeu/practica04/Practica04ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.practica04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
